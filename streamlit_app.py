8d8c3027ccbd31ab21184ad415861604bf25e33b
jetnew <notesjet@gmail.com>
2021-08-16 17:59:06 +0800

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ import streamlit as st
 from texts import *
 import altair as alt
 
+
+# Import dataframes
+df_electricity = pd.read_excel("electricity_by_area_and_building.xlsx", index_col="Area")
+df_population_size = pd.read_excel("population_size.xlsx")
+agg_elec = pd.read_excel("aggregated_electricity.xlsx", index_col='Type')
+
+
 def compute_usage(device, num, num_hours):
     if device == "aircon":
         unit = 0.8
@@ -14,11 +21,12 @@ def compute_usage(device, num, num_hours):
     car_distance = co2_monthly / 0.251087632
     return co2_monthly, cost_monthly, car_distance
 
+
 st.title(txt_title)
 
-# Part 1 
-st.header(header1a)
-st.markdown(section1a)
+# Part 1: Introduction to the Climate Emergency
+st.header(header1a)  # We are aware...
+st.markdown(section1a)  # 51 billion tons...
 st.image('images/global_energy_consumption.png')
 st.caption(caption1)
 st.caption(citation1)
@@ -41,7 +49,7 @@ st.caption(caption5)
 st.caption(citation5)
 st.markdown(section1e)
 
-# Part 2
+# Part 2: Diving into Singapore's energy consumption landscape
 
 st.header(header2)
 st.markdown(section2)
@@ -49,7 +57,7 @@ st.image('images/total_electricity_households.png')
 st.caption(caption6)
 st.caption(citation6)
 
-# Part 3
+# Part 3: Interactive Segment on Average Household Energy Use
 
 st.header(header3)
 st.markdown(section3)
@@ -57,16 +65,37 @@ st.markdown(section3)
 residential_area = st.selectbox('Select your residential area:', area)
 dwelling_type = st.selectbox('Select your housing type:', housing_type)
 
+avg_elec_consumed = df_electricity[dwelling_type][residential_area]
+st.text(f"Average Electricty Consumed: {avg_elec_consumed:.2f} kWh")
+
+with st.beta_expander(f"Optional: Compare your monthly electricity usage with the average among {residential_area} {dwelling_type}!"):
+    user_avg_elec = st.number_input("How much electricity do you use a month? (kWh)")
+
+    if dwelling_type == "Condominiums and Other Apartments" or dwelling_type == "Landed Properties":
+        residential_area = "Overall"
+        st.info(f"Note: There is only available average data for {dwelling_type}.")
+
+    avg_elec_consumed = df_electricity[dwelling_type][residential_area]
+    st.text(f"Average Electricty Consumed: {avg_elec_consumed:.2f} kWh")
+
+    st.bar_chart(pd.DataFrame({
+        'data': [user_avg_elec, avg_elec_consumed],
+        'index': ["Your monthly electricity usage", "Average monthly electricity usage"]
+    }))
+
+
+
 
 # Bargraph: Neighbourhood -> Housing type electricity consumption
 # Note: Use of wrong excel sheet
-agg_elec = pd.read_excel("aggregated_electricity.xlsx", index_col='Type')
+st.markdown("This is the average electricty consumed by housing type:")
 st.bar_chart(agg_elec['Usage'])
 
 st.caption(caption7)
 st.caption(citation7)
 
-# Part 4
+# Part 4: Interactive Segment on Aircon vs Fan
+
 st.header(header4)
 st.markdown(section4)
 st.image('images/household_energy_profile.png')
@@ -97,9 +126,8 @@ aircon_co2_monthly, aircon_cost_monthly, aircon_car_distance = compute_usage("ai
 cost_savings = abs(fan_cost_monthly - aircon_cost_monthly)
 new_monthly_cost = monthly_cost - cost_savings
 new_monthly_co2 = fan_co2_monthly + aircon_co2_monthly
-st.text(f"YOU NOW SPEND: ${new_monthly_cost:.2f}")
-st.text(f"YOU SAVE A GRAND TOTAL OF: ${cost_savings:.2f} A MONTH!")
-st.text(f"THAT'S EQUIVALENT TO {cost_savings/4} BUBBLE TEAS! :) (PER MONTH!!!)")
+st.markdown(f"You now spend ${new_monthly_cost:.2f}, and save a total of ${cost_savings:.2f} every month!")
+st.markdown(f"That's equivalent to {cost_savings/4:.0f} cups of bubble tea! :) (per month!)")
 
 
 st.bar_chart(pd.DataFrame(
@@ -115,45 +143,16 @@ st.bar_chart(pd.DataFrame(
 
 st.caption(caption10)
 
+# What happens if every household in Singapore swaps out some aircon hours for fan hours?
 st.subheader(header4c)
+sg_population_size = 1_225_300
+st.markdown(f"If everyone was as honourable as you we would save: ${cost_savings * sg_population_size:,.2f} per month!")
 
-"""
-Bar Chart: CO2 emissions saved and cost savings
-
-Bar graph - Aggregated statistics - If everyone changes from AC hours to fan hours per household
-By housing type
-"""
-
-df_electricity = pd.read_excel("electricity_by_area_and_building.xlsx", index_col="Area")
-if dwelling_type == "Condominiums and Other Apartments" or dwelling_type == "Landed Properties":
-    residential_area = "Overall"
-    st.info(f"Note: There is only available average data for {dwelling_type}.")
-st.text(f"Average Electricty Consumed: {df_electricity[dwelling_type][residential_area]:.2f}")
-
-st.text("Fan")
+reduction_co2 = monthly_co2 - new_monthly_co2
+st.markdown(f"If everyone was as honourable as you we would cut down: {reduction_co2 * sg_population_size:,} kg of CO2 emissions per month!")
 
 
-num_fans = st.number_input("No. of fans")
-num_fan_hours = st.number_input("No. of fan hours used per day")
-fan_co2_monthly, fan_cost_monthly, fan_car_distance = compute_usage("fan", num_fans, num_fan_hours)
-st.text(f"CO2 emitted per month (kg): {fan_co2_monthly:.2f}")
-st.text(f"The amount of CO2 produced is equivalent to that of a car that drove {fan_car_distance:.2f} km!")
-st.text(f"The monthly cost of your fan is: S${fan_cost_monthly:.2f}!")
-st.text("Aircon")
-num_aircon = st.number_input("No. of aircons")
-num_aircon_hours = st.number_input("No. of aircon hours used per day")
-aircon_co2_monthly, aircon_cost_monthly, aircon_car_distance = compute_usage("aircon", num_aircon, num_aircon_hours)
-st.text(f"CO2 emitted per month (kg): {aircon_co2_monthly:.2f}")
-st.text(f"The amount of CO2 produced is equivalent to that of a car that drove {aircon_car_distance:.2f} km!")  # TODO: Include footnote
-st.text(f"The monthly cost of your aircon is: S${aircon_cost_monthly:.2f}!")
-
-st.text("Average Household Size")
-household_size = [3.3, 3.3, 4.3, 2.9]
-st.bar_chart(household_size)
-
-df_population_size = pd.read_excel("population_size.xlsx")
-
-# Part 5
+# Part 5: Interactive segment on Fluorescent vs Incandescent
 
 st.header(header5)
 st.markdown(section5)
